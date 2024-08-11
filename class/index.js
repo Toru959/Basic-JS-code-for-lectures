39bbd4f0ee6658d809a2d26b00d991669feac551
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-08-11 10:57:55 +0800

    モジュール

---
+++ class/index.js
@@ -0,0 +1,9 @@
+import { Product } from './Product/product.js';
+import { Test } from './test.js';
+
+const product = new Product();
+const test = new Test();
+
+product.getProduct();
+test.getTest();
+product.getBaseProduct();
